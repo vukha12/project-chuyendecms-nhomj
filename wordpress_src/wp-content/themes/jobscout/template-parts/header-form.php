13dfc9c6c5831715e500fcae455905941518cd66
vukha12 <kha15964@gmail.com>
2025-11-26 13:22:28 +0700

    done search (yeu cau 1)

--- wordpress_src/wp-content/themes/jobscout/template-parts/header-form.php
+++ wordpress_src/wp-content/themes/jobscout/template-parts/header-form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Creating a custom job search form for homepage
@@ -8,46 +9,53 @@
  *
  * @package JobScout
  */
-$find_a_job_link = get_option( 'job_manager_jobs_page_id', 0 );
-$post_slug       = get_post_field( 'post_name', $find_a_job_link );
-$ed_job_category = get_option( 'job_manager_enable_categories' );  
+$find_a_job_link = get_option('job_manager_jobs_page_id', 0);
+$post_slug       = get_post_field('post_name', $find_a_job_link);
+$ed_job_category = get_option('job_manager_enable_categories');
 
-if( $post_slug ){
-    $action_page =  home_url( '/'. $post_slug );
-}else {
-    $action_page =  home_url( '/' );
-}
+$action_page = home_url('/jobs/');
 ?>
 
 <div class="job_listings">
 
-  <form class="jobscout_job_filters" method="GET" action="<?php echo esc_url( $action_page ) ?>">
+  <form class="jobscout_job_filters" method="GET" action="<?php echo esc_url($action_page) ?>">
     <div class="search_jobs">
 
       <div class="search_keywords">
-        <label for="search_keywords"><?php esc_html_e( 'Keywords', 'jobscout' ); ?></label>
-        <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php esc_attr_e( 'Keywords', 'jobscout' ); ?>">
+        <label for="search_keywords"><?php esc_html_e('Keywords', 'jobscout'); ?></label>
+        <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php esc_attr_e('Search for jobs, companies, skills', 'jobscout'); ?>">
       </div>
 
-      <div class="search_location">
-        <label for="search_location"><?php esc_html_e( 'Location', 'jobscout' ); ?></label>
-        <input type="text"  id="search_location" name="search_location" placeholder="<?php esc_attr_e( 'Location', 'jobscout' ); ?>">
+      <div class="search_location change">
+        <?php
+        global $wpdb;
+        $table  = $wpdb->prefix . 'postmeta';
+        $sql = "SELECT DISTINCT SUBSTRING_INDEX(`meta_value`,',',-1) as location FROM `wp_postmeta` WHERE `meta_key` like '%location%' ORDER BY location";
+        $data = $wpdb->get_results($wpdb->prepare($sql));
+        ?>
+
+        <select id="search_location" name="search_location" value="Khu vực">
+          <option value="">Khu vực</option>
+          <?php foreach ($data as $value) : ?>
+            <option value="<?php echo $value->location; ?>"><?php echo $value->location; ?></option>
+          <?php endforeach ?>
+        </select>
       </div>
-      
-      <?php if( $ed_job_category ){ ?>
-          <div class="search_categories custom_search_categories">
-            <label for="search_category"><?php esc_html_e( 'Job Category', 'jobscout' ); ?></label>
-            <select id="search_category" class="robo-search-category" name="search_category">
-            <option value=""><?php _e( 'Select Job Category', 'jobscout' ); ?></option>
-              <?php foreach ( get_job_listing_categories() as $jobcat ) : ?>
-                <option value="<?php echo esc_attr( $jobcat->term_id ); ?>"><?php echo esc_html( $jobcat->name ); ?></option>
-              <?php endforeach; ?>
-            </select>
-          </div>
+
+      <?php if ($ed_job_category) { ?>
+        <div class="search_categories custom_search_categories">
+          <label for="search_category"><?php esc_html_e('Job Category', 'jobscout'); ?></label>
+          <select id="search_category" class="robo-search-category" name="search_category">
+            <option value=""><?php _e('Select Job Category', 'jobscout'); ?></option>
+            <?php foreach (get_job_listing_categories() as $jobcat) : ?>
+              <option value="<?php echo esc_attr($jobcat->term_id); ?>"><?php echo esc_html($jobcat->name); ?></option>
+            <?php endforeach; ?>
+          </select>
+        </div>
       <?php } ?>
-      
+
       <div class="search_submit">
-        <input type="submit" value="<?php esc_attr_e( 'Search', 'jobscout'); ?>" />
+        <input type="submit" value="<?php esc_attr_e('SEARCH JOB', 'jobscout'); ?>">
       </div>
 
     </div>
