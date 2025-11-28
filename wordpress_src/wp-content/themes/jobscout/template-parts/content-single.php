44a3d70a0b803a6b01682a7d63c4213eacbaa90e
vukha12 <kha15964@gmail.com>
2025-11-28 11:26:57 +0700

    done

--- wordpress_src/wp-content/themes/jobscout/template-parts/content-single.php
+++ wordpress_src/wp-content/themes/jobscout/template-parts/content-single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -10,20 +11,20 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php 
-        /**
-         * 
-         * @hooked jobscout_entry_header - 10
-         * @hooked jobscout_post_thumbnail - 15
-        */
-        do_action( 'jobscout_before_single_post_entry_content' );
-    
+    <?php
+    /**
+     * 
+     * @hooked jobscout_entry_header - 10
+     * @hooked jobscout_post_thumbnail - 15
+     */
+    do_action('jobscout_before_single_post_entry_content');
 
-        /**
-         * @hooked jobscout_entry_content - 15
-         * @hooked jobscout_entry_footer  - 20
-        */
-        do_action( 'jobscout_single_post_entry_content' );
-        
+
+    /**
+     * @hooked jobscout_entry_content - 15
+     * @hooked jobscout_entry_footer  - 20
+     */
+    do_action('jobscout_single_post_entry_content');
+
     ?>
 </article><!-- #post-<?php the_ID(); ?> -->
--- wordpress_src/wp-content/themes/jobscout/template-parts/content.php
+++ wordpress_src/wp-content/themes/jobscout/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -8,20 +9,20 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemtype="https://schema.org/Blog">
-	<?php 
-        /**
-         * @hooked jobscout_post_thumbnail - 10
-        */
-        do_action( 'jobscout_before_post_entry_content' );
+    <?php
+    /**
+     * @hooked jobscout_post_thumbnail - 10
+     */
+    do_action('jobscout_before_post_entry_content');
 
-        echo '<div class="content-wrap">';
-        /**
-         * @hooked jobscout_entry_header  - 10 
-         * @hooked jobscout_entry_content - 15
-         * @hooked jobscout_entry_footer  - 20
-        */
-        do_action( 'jobscout_post_entry_content' );
-        
-        echo '</div>';
+    echo '<div class="content-wrap">';
+    /**
+     * @hooked jobscout_entry_header  - 10 
+     * @hooked jobscout_entry_content - 15
+     * @hooked jobscout_entry_footer  - 20
+     */
+    do_action('jobscout_post_entry_content');
+
+    echo '</div>';
     ?>
 </article><!-- #post-<?php the_ID(); ?> -->
